b967c7f95cccbc45fb6ad9f0c893aa01ec840f6c
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-06-02 17:46:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('novoItem')
+const lista = document.getElementById('lista');
+
+form.addEventListener('submit', (evento) => {
+  evento.preventDefault()
+
+  criarElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+
+function criarElemento(nome, quantidade) {
+  // <li class="item"><strong>10</strong>Cuecas</li>
+  const novoItem = document.createElement('li');
+  novoItem.classList.add('item');
+
+  const numeroItem = document.createElement('strong');
+  numeroItem.innerHTML = quantidade;
+
+  novoItem.appendChild(numeroItem);
+  novoItem.innerHTML += nome;
+
+  lista.appendChild(novoItem);
+}
