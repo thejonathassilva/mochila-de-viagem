4a1c2ee065f5f634764995083aabf37ab3e89e2a
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-06-04 13:12:19 -0300

    finalização de curso

--- js/main.js
+++ js/main.js
@@ -22,9 +22,9 @@ form.addEventListener('submit', (evento) => {
     itemAtual.id = existe.id;
     atualizaElemento(itemAtual);
 
-    itens[existe.id] = itemAtual;
+    itens[itens.findIndex(elemento => elemento.id === itemAtual.id)] = itemAtual;
   } else {
-    itemAtual.id = itens.length;
+    itemAtual.id = itens[itens.length -1] ? itens[itens.length -1].id + 1 : 0;
     criarElemento(itemAtual)
   
     itens.push(itemAtual);
@@ -47,10 +47,31 @@ function criarElemento(item) {
 
   novoItem.innerHTML += item.nome;
 
+  novoItem.appendChild(botaoDeleta(item.id))
+
   lista.appendChild(novoItem);
    
 }
 
 function atualizaElemento(item) {
   document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+  const elementoBotao = document.createElement('button');
+  elementoBotao.innerText = 'X';
+
+  elementoBotao.addEventListener('click', function() {
+    deletaElemento(this.parentNode, id);
+  })
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+  tag.remove();
+
+  itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+  localStorage.setItem('itens', JSON.stringify(itens));
 }
